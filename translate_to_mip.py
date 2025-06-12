749c8416bd0452c123473622563d67491f740c7a
Raoul Luqué <raoulsluque@gmail.com>
2025-06-12 14:54:52 +0200

    Sync current faulty state of translate_to_mip.py

--- translate_to_mip.py
+++ translate_to_mip.py
@@ -5,7 +5,8 @@ import matplotlib.pyplot as plt
 import networkx as nx
 from networkx import DiGraph
 
-from encoding import Vehicle, TruckIdentifier, Truck, Location, LocationType, TruckAssignment, VehicleAssignment
+from encoding import Vehicle, TruckIdentifier, Truck, Location, LocationType, TruckAssignment, VehicleAssignment, \
+    FIXED_UNPLANNED_DELAY_COST, FIXED_PLANNED_DELAY_COST, COST_PER_UNPLANNED_DELAY_DAY, COST_PER_PLANNED_DELAY_DAY
 from datetime import timedelta, date
 
 
@@ -22,8 +23,12 @@ class NodeType(Enum):
 
 @dataclass(frozen=True)
 class NodeIdentifier:
+    """
+
+    """
     day: date
     location: Location
+    type: NodeType
 
 
 def solve_as_mip(vehicles: list[Vehicle], trucks: dict[TruckIdentifier, Truck], locations: list[Location]) -> (
@@ -40,56 +45,112 @@ def solve_as_mip(vehicles: list[Vehicle], trucks: dict[TruckIdentifier, Truck], 
         tuple: A tuple containing the list of locations, vehicles, and trucks. The trucks and vehicles are adjusted
         to contain their respective plans.
     """
-    # Create a list of all days we are considering. The first day is day 0 and the day where the first vehicle is available
-    # to be transported from a production site.
+    # Set a parameter representing unbounded capacity
+    UNBOUNDED = float('inf')
+
+    # Create a list of all days we are considering. The first day is day 0 and the day when the first vehicle is available
     first_day: date = min(min(vehicles, key=lambda vehicle: vehicle.available_date).available_date,
                           min(trucks.values(), key=lambda truck: truck.departure_date).departure_date)
     last_day: date = max(max(vehicles, key=lambda vehicle: vehicle.available_date).available_date,
                          max(trucks.values(), key=lambda truck: truck.arrival_date).arrival_date)
+    current_day = first_day
 
     number_of_days = (last_day - first_day).days + 1
     days = [first_day + timedelta(days=i) for i in range(number_of_days)]
 
     # Create a Network to model the flow
-    flow_network = DiGraph()
+    flow_network: DiGraph[NodeIdentifier] = DiGraph()
 
     # Create the vertices of the flow network behind the MIP
     # Create a node for each day and each location
     for day in days:
         for location in locations:
-            flow_network.add_node(NodeIdentifier(day, location), day=day, location=location, flow=0)
+            flow_network.add_node(NodeIdentifier(day, location, NodeType.NORMAL), demand=0)
 
     # Adjust the flow of each node according to the vehicles produced and expected on that day
     for vehicle in vehicles:
-        flow_network.nodes[NodeIdentifier(vehicle.available_date, vehicle.origin)]['flow'] += 1
-        flow_network.nodes[NodeIdentifier(vehicle.due_date, vehicle.destination)]['flow'] -= 1
+        # A positive demand indicates that flow should end there, reverse for negative
+        flow_network.nodes[NodeIdentifier(vehicle.available_date, vehicle.origin, NodeType.NORMAL)]['demand'] -= 1
+        flow_network.nodes[NodeIdentifier(vehicle.due_date, vehicle.destination, NodeType.NORMAL)]['demand'] += 1
 
     # Create the edges of the flow network for the trucks
     for truck in trucks.values():
-        start_node = NodeIdentifier(truck.departure_date, truck.start_location)
-        end_node = NodeIdentifier(truck.arrival_date, truck.end_location)
+        start_node = NodeIdentifier(truck.departure_date, truck.start_location, NodeType.NORMAL)
+        end_node = NodeIdentifier(truck.arrival_date, truck.end_location, NodeType.NORMAL)
 
         # Add an edge from the start node to the end node with the truck's capacity as the flow
-        flow_network.add_edge(start_node, end_node, capacity=truck.capacity, price=truck.price)
+        flow_network.add_edge(start_node, end_node, capacity=truck.capacity, weight=truck.price)
 
-    # Create the helper edges for the flow network
+    # Create the helper edges for the flow network connecting the columns
     for day in days:
         for location in locations:
-            current_node = NodeIdentifier(day, location)
+            current_node = NodeIdentifier(day, location, NodeType.NORMAL)
             # Add edges to the next day for each location
             if day < last_day:
                 # Create an edge to the next day node
-                next_day_node = NodeIdentifier(day + timedelta(days=1), location)
-                flow_network.add_edge(current_node, next_day_node, capacity=float('inf'), price=0)
+                next_day_node = NodeIdentifier(day + timedelta(days=1), location, NodeType.NORMAL)
+                flow_network.add_edge(current_node, next_day_node, capacity=UNBOUNDED, weight=0)
 
             # Add helper edges at dealer locations to allow for delays
             if location.type == LocationType.DEALER:
                 # Create an edge to the next day node with a delay of 1 day
-                next_day_node = NodeIdentifier(day + timedelta(days=1), location)
-                flow_network.add_edge(current_node, next_day_node, capacity=float('inf'), price=0)
+                next_day_node = NodeIdentifier(day + timedelta(days=1), location, NodeType.NORMAL)
+                flow_network.add_edge(current_node, next_day_node, capacity=UNBOUNDED, weight=0)
 
-    visualize_flow_graph(flow_network, first_day, locations)
+    # Create the helper nodes for each DEALER location
+    for day in days:
+        for location in locations:
+            if location.type == LocationType.DEALER:
+                # Add the first helper node
+                current_helper_node_one = NodeIdentifier(day, location, NodeType.HELPER_NODE_ONE)
+                flow_network.add_node(current_helper_node_one)
 
+                # Distinguish case of first 7 days including current_day
+                if day < current_day + timedelta(days=7):
+                    # Add edges to first helper node (UNPLANNED DELAY, since we are in the first 7 days)
+                    current_normal_node = NodeIdentifier(day, location, NodeType.NORMAL)
+                    flow_network.add_edge(current_normal_node, current_helper_node_one, capacity=UNBOUNDED,
+                                          weight=FIXED_UNPLANNED_DELAY_COST)
+                    flow_network.add_edge(current_helper_node_one, current_normal_node, capacity=UNBOUNDED, weight=0)
+                    if day != first_day:
+                        # Add an edge to the HELPER_NODE_ONE above
+                        previous_helper_node_one = NodeIdentifier(day - timedelta(days=1), location,
+                                                                  NodeType.HELPER_NODE_ONE)
+                        flow_network.add_edge(current_helper_node_one, previous_helper_node_one, capacity=UNBOUNDED,
+                                              weight=COST_PER_UNPLANNED_DELAY_DAY)
+                else:
+                    # Add edges to first helper node (PLANNED DELAY, since we are after the first 7 days)
+                    current_normal_node = NodeIdentifier(day, location, NodeType.NORMAL)
+                    flow_network.add_edge(current_normal_node, current_helper_node_one, capacity=UNBOUNDED,
+                                          weight=FIXED_PLANNED_DELAY_COST)
+                    flow_network.add_edge(current_helper_node_one, current_normal_node, capacity=UNBOUNDED, weight=0)
+
+                    # Add the second helper node and an edge to it
+                    current_helper_node_two = NodeIdentifier(day, location, NodeType.HELPER_NODE_TWO)
+                    flow_network.add_edge(current_normal_node, current_helper_node_two, capacity=UNBOUNDED,
+                                          weight=FIXED_UNPLANNED_DELAY_COST)
+
+                    # Distinguish 8th day or not
+                    if day != current_day + timedelta(days=7):
+                        # Add edges connecting current HELPER_NODE_ONE and _TWO to the previous days' nodes respectively
+                        previous_helper_node_one = NodeIdentifier(day - timedelta(days=1), location,
+                                                                  NodeType.HELPER_NODE_ONE)
+                        previous_helper_node_two = NodeIdentifier(day - timedelta(days=1), location,
+                                                                  NodeType.HELPER_NODE_TWO)
+                        flow_network.add_edge(current_helper_node_one, previous_helper_node_one, capacity=UNBOUNDED,
+                                              weight=COST_PER_PLANNED_DELAY_DAY)
+                        flow_network.add_edge(current_helper_node_two, previous_helper_node_two, capacity=UNBOUNDED,
+                                              weight=COST_PER_UNPLANNED_DELAY_DAY)
+
+                    else:
+                        # Add only an edge from the current HELPER_NODE_TWO to the HELPER_NODE_ONE from the previous day
+                        previous_helper_node_one = NodeIdentifier(day - timedelta(days=1), location,
+                                                                  NodeType.HELPER_NODE_ONE)
+                        flow_network.add_edge(current_helper_node_two, previous_helper_node_one, capacity=UNBOUNDED,
+                                              weight=COST_PER_UNPLANNED_DELAY_DAY)
+
+    # visualize_flow_graph(flow_network, first_day, locations)
+    print(nx.min_cost_flow_cost(flow_network))
     return [], {}
 
 
@@ -98,8 +159,13 @@ def visualize_flow_graph(flow_network: DiGraph, first_day: date, locations: list
     Visualizes the flow network using matplotlib and networkx.
 
     Args:
-        flow_network (DiGraph): The flow network to visualize.
+        flow_network (DiGraph[NodeIdentifier]): The flow network to visualize.
+        first_day (date):
+        locations: (list[Location]):
     """
+    # Type magic
+    flow_network: DiGraph[NodeIdentifier] = flow_network
+
     pos = {}
 
     # Position the nodes for ascending days and the same location in one column
@@ -110,7 +176,7 @@ def visualize_flow_graph(flow_network: DiGraph, first_day: date, locations: list
         pos[node] = (locations.index(location) * scale, -(day.toordinal() - first_day.toordinal() * scale))
 
     # Customize the labels
-    labels = {node: f"{node.location.name[:5]}_Day{(node.day - first_day).days}" for node in flow_network.nodes}
+    labels = {node: f"{node.location.name[:5]}_Day{node.day}" for node in flow_network.nodes}
 
     # Draw the flow network
     plt.figure(figsize=(12, 24), dpi=300)
