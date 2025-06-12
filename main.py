b06d20fde3df896562fab56ce7fcaf36d0da60db
Raoul Luqué <raoulsluque@gmail.com>
2025-06-12 10:02:23 +0200

    Add basic main.py

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
-# This is a sample Python script.
-from encoding import Location, TruckIdentifier, Vehicle, Truck
+from encoding import Location, TruckIdentifier, Vehicle, Truck, LocationType
+from parsing import read_data
+from translate_to_mip import solve_as_mip
 
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-def read_data() -> tuple[list[Location], list[Vehicle], dict[TruckIdentifier, Truck]]:
-    """
-    Reads the data from the files and returns the lists of locations, vehicles, and trucks.
-    :return: A tuple containing the lists of locations, vehicles, and trucks.
-    """
-    return [], [], {}
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    locations, vehicles, trucks = read_data()
+    solve_as_mip(vehicles, trucks, locations)
