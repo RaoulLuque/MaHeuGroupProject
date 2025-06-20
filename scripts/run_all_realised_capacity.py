a0c7d01caf3500a2b71da215fe677d69c970c0c4
Raoul Luqué <raoulsluque@gmail.com>
2025-06-20 14:39:10 +0200

    Move data to new folder to comply with new data organization

--- scripts/run_all_realised_capacity.py
+++ scripts/run_all_realised_capacity.py
@@ -15,11 +15,13 @@ def run_on_all_data_from_first_dataset():
     os.makedirs(results_dir, exist_ok=True)
     current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     with open(os.path.join(results_dir, f"{current_time}_result.txt"), 'w') as result_file:
-        data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../data'))
+        dataset_dir = "CaseMaHeu25_01"
+        data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', dataset_dir))
         pattern = os.path.join(data_dir, 'realised_capacity_data_*.csv')
         files = sorted(glob.glob(pattern))
         for file in files:
-            locations, vehicles, trucks = read_data(capacity_data=file)
+            file = os.path.join(dataset_dir, file)
+            locations, vehicles, trucks = read_data(realised_capacity_file_name=file)
             vehicle_assignment, truck_assignment = solve_as_flow(vehicles, trucks, locations)
             cost = objective_function(vehicle_assignment, truck_assignment, trucks)
             output = f"Cost of solution for {os.path.basename(file)}: {cost:.2f} \n"
