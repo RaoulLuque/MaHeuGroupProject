31605335c2ace3b61061b0b87160d735620eb3c2
Raoul Luqué <raoulsluque@gmail.com>
2025-06-23 20:26:15 +0200

    Change script to be able to run multiple solvers one after the other

--- scripts/run_all_realised_capacity.py
+++ scripts/run_all_realised_capacity.py
@@ -13,45 +13,46 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../src'
 from maheu_group_project.solution.evaluate import objective_function
 
 # This is the solver to be used/tested
-SOLVER = SolverType.FLOW
+SOLVERS = [ SolverType.LOWER_BOUND_UNCAPACITATED_FLOW, SolverType.FLOW, SolverType.OLD_FLOW]
 
 
 def run_on_all_data_from_first_dataset():
-    results_dir = os.path.join(os.path.dirname(__file__), '..', 'results')
-    os.makedirs(results_dir, exist_ok=True)
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    with open(os.path.join(results_dir, f"{current_time}_result.txt"), 'w') as result_file:
-        with open(os.path.join(results_dir, f"{current_time}_result_pretty.txt"), 'w') as pretty_result_file:
-            dataset_dir = "CaseMaHeu25_01"
-            data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', dataset_dir))
-            pattern = os.path.join(data_dir, 'realised_capacity_data_*.csv')
-            files = sorted(glob.glob(pattern))
-            for file in files:
-                vehicle_assignments, truck_assignments, _, vehicles, trucks_realised, _ = solve_deterministically_and_return_data(
-                    SOLVER,
-                    dataset_dir,
-                    os.path.basename(file)
-                )
-                is_valid = verify_solution(vehicles, vehicle_assignments, trucks_realised, truck_assignments)
-                number_of_vehicles_that_did_not_arrived = 0
-                match is_valid:
-                    case bool():
-                        assert is_valid, "The solution is invalid"
-                    case int():
-                        number_of_vehicles_that_did_not_arrived = is_valid
-                    case _:
-                        raise TypeError(f"Unexpected type of is_valid: {type(is_valid)}")
-                cost = objective_function(vehicle_assignments, truck_assignments, trucks_realised)
-                output = f"Cost of solution for {dataset_dir}/{os.path.basename(file)}: {cost:.2f} \n"
-                if number_of_vehicles_that_did_not_arrived != 0:
-                    output += f"Number of vehicles that did not arrive: {number_of_vehicles_that_did_not_arrived} \n"
-                result_file.write(output)
-                if number_of_vehicles_that_did_not_arrived == 0:
-                    output += f"Number of vehicles that did not arrive: {number_of_vehicles_that_did_not_arrived} \n"
-                metrics = get_pretty_metrics(trucks_realised, truck_assignments, vehicle_assignments)
-                output += metrics + "\n" + "\n"
-                pretty_result_file.write(output)
-                print(output)
+    for solver in SOLVERS:
+        results_dir = os.path.join(os.path.dirname(__file__), '..', 'results')
+        os.makedirs(results_dir, exist_ok=True)
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        with open(os.path.join(results_dir, f"{current_time}_{solver}_result.txt"), 'w') as result_file:
+            with open(os.path.join(results_dir, f"{current_time}_{solver}_result_pretty.txt"), 'w') as pretty_result_file:
+                dataset_dir = "CaseMaHeu25_01"
+                data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', dataset_dir))
+                pattern = os.path.join(data_dir, 'realised_capacity_data_*.csv')
+                files = sorted(glob.glob(pattern))
+                for file in files:
+                    vehicle_assignments, truck_assignments, _, vehicles, trucks_realised, _ = solve_deterministically_and_return_data(
+                        solver,
+                        dataset_dir,
+                        os.path.basename(file)
+                    )
+                    is_valid = verify_solution(vehicles, vehicle_assignments, trucks_realised, truck_assignments)
+                    number_of_vehicles_that_did_not_arrived = 0
+                    match is_valid:
+                        case bool():
+                            assert is_valid, "The solution is invalid"
+                        case int():
+                            number_of_vehicles_that_did_not_arrived = is_valid
+                        case _:
+                            raise TypeError(f"Unexpected type of is_valid: {type(is_valid)}")
+                    cost = objective_function(vehicle_assignments, truck_assignments, trucks_realised)
+                    output = f"Cost of solution for {dataset_dir}/{os.path.basename(file)}: {cost:.2f} \n"
+                    if number_of_vehicles_that_did_not_arrived != 0:
+                        output += f"Number of vehicles that did not arrive: {number_of_vehicles_that_did_not_arrived} \n"
+                    result_file.write(output)
+                    if number_of_vehicles_that_did_not_arrived == 0:
+                        output += f"Number of vehicles that did not arrive: {number_of_vehicles_that_did_not_arrived} \n"
+                    metrics = get_pretty_metrics(trucks_realised, truck_assignments, vehicle_assignments)
+                    output += metrics + "\n" + "\n"
+                    pretty_result_file.write(output)
+                    print(output)
 
 
 if __name__ == '__main__':
