9c219a9eaa6f73b6631cfcbec4aaabd422f6be0d
Tom Bürskens <tbuerskens@gmx.de>
2025-06-29 02:04:28 +0200

    updated structure of greedy_candidate_paths

--- src/maheu_group_project/heuristics/greedy/greedy_candidate_paths.py
+++ src/maheu_group_project/heuristics/greedy/greedy_candidate_paths.py
@@ -19,78 +19,55 @@ def greedy_candidate_path_solver(requested_vehicles: list[Vehicle], trucks_plann
     """
     first_day, last_day, days = get_first_last_and_days(vehicles=requested_vehicles, trucks=trucks_planned)
     day_of_planning = first_day  # today (is relevant for planned delay calculation)
-    # location_list: list[Location] = list(set(loc for path in shortest_paths.values() for loc in path))
+    expected_travel_time: dict[tuple[Location, Location], timedelta] = {(loc, vehicle.destination): timedelta(5) for
+                                                                        vehicle in requested_vehicles for loc in
+                                                                        location_list}
+    # TODO: Aus candidate_paths ziehen
     vehicles_at_loc_at_time: dict[tuple[Location, date], list[int]] = {(loc, day): [] for loc in location_list for day
-                                                                       in (days + [(last_day + timedelta(
-            1))])}  # Include the next day to allow for vehicles to stay at a location for another day
+                                                                       in (days + [(last_day + timedelta(1))])}
     # Run first with only expected trucks to preview delays
-    planned_vehicle_assignments: list[VehicleAssignment] = [
-        VehicleAssignment(vehicle.id, [], False, timedelta(0)) for vehicle
-        in requested_vehicles]
-    for day in days:  # days from start_date to end_date)
+    planned_vehicle_assignments: list[VehicleAssignment] = [VehicleAssignment(vehicle.id, [],
+                                                                              False, timedelta(0)) for vehicle in
+                                                            requested_vehicles]
+    planned_truck_assignments: dict[TruckIdentifier, TruckAssignment] = {truck_id: TruckAssignment() for truck_id in
+                                                                         (
+                                                                                     trucks_planned.keys() | trucks_realised.keys())}
+    for day in days:  # days from start_date to end_date
         for loc in location_list:
             # for every day and every location, if that location is a PLANT, add vehicles that become available there
             if loc.type == location_type_from_string("PLANT"):
                 vehicles_at_loc_at_time[(loc, day)] += [vehicle.id for vehicle in requested_vehicles if
                                                         vehicle.origin == loc and vehicle.available_date == day]
-            nextloc_partitions = {}  # Partition the list of vehicles at the current location by what their next location is
-            for vehicle_id in vehicles_at_loc_at_time[(loc, day)]:
-                # sort vehicles by next loc
+            sorted_vehicles_at_loc_at_time = sorted(vehicles_at_loc_at_time[(loc, day)],
+                                                    key=lambda vehicle_id: requested_vehicles[vehicle_id].due_date -
+                                                                           expected_travel_time[loc, requested_vehicles[
+                                                                               vehicle_id].destination])  # TODO: anpassen
+            for vehicle_id in sorted_vehicles_at_loc_at_time:
                 vehicle = requested_vehicles[vehicle_id]
-                if vehicle.destination != loc:
-                    # If the vehicle's destination is not the current location, it needs to be sent onwards
-                    vehicle_path = shortest_paths[vehicle.origin, vehicle.destination]
-                    next_loc = vehicle_path[vehicle_path.index(loc) + 1]
-                    if next_loc not in nextloc_partitions:
-                        # If the next location for a vehicle is not yet a key in the partition, add it
-                        nextloc_partitions[next_loc] = []
-                    # Add the vehicle to the partition for its next location
-                    nextloc_partitions[next_loc].append(vehicle)
-            for next_loc, partition in nextloc_partitions.items():
-                # For every next location create a list of trucks that is expected to depart today from the current location to the next location
-                truck_id_list = []
-                for truck_id in trucks_planned.keys():
-                    if truck_id.start_location == loc and truck_id.end_location == next_loc and truck_id.departure_date == day:
-                        truck_id_list.append(truck_id)
-                # sort trucks by price
-                sorted_truck_id_list = sorted(truck_id_list, key=lambda truck_id: trucks_planned[truck_id].price)
-                # sort vehicles by due date
-                sorted_partition = sorted(partition, key=lambda vehicle: vehicle.due_date)
-                vehicle_amount = len(sorted_partition)
-                # decide how many trucks to book based on expected truck list
-                total_capacity = 0
-                final_truck_id = None
-                for truck_id in sorted_truck_id_list:
-                    total_capacity += trucks_planned[truck_id].capacity
-                    if total_capacity >= vehicle_amount:
-                        final_truck_id = truck_id
-                        break
-                # assign all vehicles in the current partition to trucks
-                vehicle_index = 0
-                for truck_id in sorted_truck_id_list:
-                    truck = trucks_planned[truck_id]
-                    current_truck_load = 0  # len(truck_assignments[truck_id].load)
-                    capacity = truck.capacity
-                    while current_truck_load < capacity:
-                        # While the truck is not full, assign vehicles to it
-                        vehicle_id = sorted_partition[vehicle_index].id
-                        planned_vehicle_assignments[vehicle_id].paths_taken.append(truck_id)
-                        vehicles_at_loc_at_time[(next_loc, truck.arrival_date + timedelta(1))].append(vehicle_id)
-                        current_truck_load += 1
-                        vehicle_index += 1
-                        if vehicle_index >= vehicle_amount:
-                            # If all vehicles in the partition are assigned, break
-                            break
-                    if vehicle_index >= vehicle_amount:
-                        # If all vehicles in the partition are assigned, break
-                        break
-                    if final_truck_id == truck_id:
-                        # If we have loaded the final truck that was booked, break
-                        break
-                while vehicle_index < vehicle_amount:
-                    # All remaining vehicles remain at the current location for another day
-                    vehicles_at_loc_at_time[(loc, day + timedelta(1))].append(sorted_partition[vehicle_index].id)
-                    vehicle_index += 1
+                if vehicle.destination == loc:
+                    # If the vehicle's destination is the current location, we can measure its delay
+                    planned_vehicle_assignments[vehicle_id].delayed_by = max(timedelta(0),
+                                                                             day - timedelta(1) - vehicle.due_date)
+                else:
+                    # determine how patient we are
+                    patient = True  # TODO: anpassen
+                    assigned = False
+                    for truck_option in candidate_paths[loc, vehicle.destination]:
+                        if truck_option[2] or not patient:
+                            truck_id = TruckIdentifier(loc, vehicle.destination, truck_option[1], day)
+                            if truck_id in trucks_planned.keys():
+                                truck = trucks_planned[truck_id]
+                                if len(planned_truck_assignments[truck_id].load) < truck.capacity:
+                                    # If the truck is not full, assign the vehicle to it
+                                    planned_truck_assignments[truck_id].load.append(vehicle_id)
+                                    planned_vehicle_assignments[vehicle_id].paths_taken.append(truck_id)
+                                    vehicles_at_loc_at_time[
+                                        (truck_option[0], truck.arrival_date + timedelta(1))].append(vehicle_id)
+                                    assigned = True
+                                break
+                    if not assigned:
+                        # If no truck was assigned, the vehicle remains at the current location for another day
+                        vehicles_at_loc_at_time[(loc, day + timedelta(1))].append(vehicle_id)
     planned_delayed_vehicles: list[bool] = [False for _ in requested_vehicles]
     # Check which vehicles are delayed based on the planned vehicle assignments
     for vehicle_assignment in planned_vehicle_assignments:
@@ -112,85 +89,41 @@ def greedy_candidate_path_solver(requested_vehicles: list[Vehicle], trucks_plann
                                                                  (trucks_planned.keys() | trucks_realised.keys())}
     vehicles_at_loc_at_time: dict[tuple[Location, date], list[int]] = {(loc, day): [] for loc in location_list for day
                                                                        in (days + [(last_day + timedelta(1))])}
-
     for day in days:  # days from start_date to end_date
         for loc in location_list:
             # for every day and every location, if that location is a PLANT, add vehicles that become available there
             if loc.type == location_type_from_string("PLANT"):
                 vehicles_at_loc_at_time[(loc, day)] += [vehicle.id for vehicle in requested_vehicles if
                                                         vehicle.origin == loc and vehicle.available_date == day]
-            nextloc_partitions = {}  # Partition the list of vehicles at the current location by what their next location is
-            for vehicle_id in vehicles_at_loc_at_time[(loc, day)]:
-                # sort vehicles by next loc
+            sorted_vehicles_at_loc_at_time = sorted(vehicles_at_loc_at_time[(loc, day)],
+                                                    key=lambda vehicle_id: requested_vehicles[vehicle_id].due_date -
+                                                                           expected_travel_time[loc, requested_vehicles[
+                                                                               vehicle_id].destination])
+            # TODO: delay time einbeziehen in urgency
+            for vehicle_id in sorted_vehicles_at_loc_at_time:
                 vehicle = requested_vehicles[vehicle_id]
                 if vehicle.destination == loc:
                     # If the vehicle's destination is the current location, we can measure its delay
                     vehicle_assignments[vehicle_id].delayed_by = max(timedelta(0),
                                                                      day - timedelta(1) - vehicle.due_date)
                 else:
-                    vehicle_path = candidate_paths[loc, vehicle.destination]
-                    next_loc = vehicle_path[vehicle_path.index(loc) + 1]
-                    if next_loc not in nextloc_partitions:
-                        # If the next location for a vehicle is not yet a key in the partition, add it
-                        nextloc_partitions[next_loc] = []
-                    # Add the vehicle to the partition for its next location
-                    nextloc_partitions[next_loc].append(vehicle)
-            for next_loc, partition in nextloc_partitions.items():
-                # For every next location create a list of trucks that is expected to depart today from the current location to the next location
-                truck_id_list = []
-                for truck_id in trucks_planned.keys():
-                    if truck_id.start_location == loc and truck_id.end_location == next_loc and truck_id.departure_date == day:
-                        truck_id_list.append(truck_id)
-                # sort trucks by price
-                sorted_truck_id_list = sorted(truck_id_list, key=lambda truck_id: trucks_planned[truck_id].price)
-                # sort vehicles by due date
-                sorted_partition = sorted(partition, key=lambda vehicle: vehicle.due_date)
-                vehicle_amount = len(sorted_partition)
-                # decide how many trucks to book based on expected truck list
-                total_capacity = 0
-                final_truck_id = None
-                for truck_id in sorted_truck_id_list:
-                    total_capacity += trucks_planned[truck_id].capacity
-                    if total_capacity >= vehicle_amount:
-                        final_truck_id = truck_id
-                        break
-                # assign all vehicles in the current partition to trucks
-                vehicle_index = 0
-                for truck_id in sorted_truck_id_list:
-                    # Check if the truck actually exists
-                    if truck_id in trucks_realised:
-                        truck = trucks_realised[truck_id]
-                        current_truck_load = 0  # len(truck_assignments[truck_id].load)
-                        capacity = truck.capacity
-                        while current_truck_load < capacity:
-                            # While the truck is not full, assign vehicles to it
-                            vehicle_id = sorted_partition[vehicle_index].id
-                            truck_assignments[truck_id].load.append(vehicle_id)
-                            vehicle_assignments[vehicle_id].paths_taken.append(truck_id)
-                            vehicles_at_loc_at_time[(next_loc, truck.arrival_date + timedelta(1))].append(vehicle_id)
-                            current_truck_load += 1
-                            vehicle_index += 1
-                            if vehicle_index >= vehicle_amount:
-                                # If all vehicles in the partition are assigned, break
+                    # determine how patient we are
+                    patient = True  # TODO: Funktion schreiben
+                    assigned = False
+                    for truck_option in candidate_paths[loc, vehicle.destination]:
+                        if truck_option[2] or not patient:
+                            truck_id = TruckIdentifier(loc, vehicle.destination, truck_option[1], day)
+                            if truck_id in (trucks_planned.keys() & trucks_realised.keys()):
+                                truck = trucks_realised[truck_id]
+                                if len(truck_assignments[truck_id].load) < truck.capacity:
+                                    # If the truck is not full, assign the vehicle to it
+                                    truck_assignments[truck_id].load.append(vehicle_id)
+                                    vehicle_assignments[vehicle_id].paths_taken.append(truck_id)
+                                    vehicles_at_loc_at_time[
+                                        (truck_option[0], truck.arrival_date + timedelta(1))].append(vehicle_id)
+                                    assigned = True
                                 break
-                    if vehicle_index >= vehicle_amount:
-                        # If all vehicles in the partition are assigned, break
-                        break
-                    if final_truck_id == truck_id:
-                        # If we have loaded the final truck that was booked, break
-                        break
-                while vehicle_index < vehicle_amount:
-                    # All remaining vehicles remain at the current location for another day
-                    vehicles_at_loc_at_time[(loc, day + timedelta(1))].append(sorted_partition[vehicle_index].id)
-                    vehicle_index += 1
+                    if not assigned:
+                        # If no truck was assigned, the vehicle remains at the current location for another day
+                        vehicles_at_loc_at_time[(loc, day + timedelta(1))].append(vehicle_id)
     return vehicle_assignments, truck_assignments
-
-
-for truck_sequence in truck_sequences[(vehicle.origin, vehicle.destination)]:
-    # Take first path that has capacity
-    if truck_sequence.capacity() > 0:
-        # assign vehicle to candidate_path
-        for truck_id in candidate_path.truck_ids:
-            vehicle_assignments[vehicle_id].paths_taken.append(truck_id)
-            truck_assignments[truck_id].load.append(vehicle_id)
-        break
