a40bc42968668605957c91fab00dd912a1b556a8
Raoul Luqué <raoulsluque@gmail.com>
2025-07-01 20:08:50 +0200

    Improve copy_from_hpc.sh script

--- scripts/copy_from_hpc.sh
+++ scripts/copy_from_hpc.sh
@@ -1,1 +1,2 @@
-scp -r xh700552@copy23-1.hpc.itc.rwth-aachen.de:/hpcwork/xh700552/MaHeuGroupProject/results ./hpc
+scp -r xh700552@copy23-1.hpc.itc.rwth-aachen.de:/hpcwork/xh700552/MaHeuGroupProject/results/real_time ./results/hpc_results/real_time
+scp -r xh700552@copy23-1.hpc.itc.rwth-aachen.de:/hpcwork/xh700552/MaHeuGroupProject/results/deterministic ./results/hpc_results/deterministic
