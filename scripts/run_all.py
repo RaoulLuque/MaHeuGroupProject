a153d8ae99befdc6173a07e85380c1279a072ca0
Raoul Luqué <raoulsluque@gmail.com>
2025-07-01 17:42:51 +0200

    Update plots and scripts to support plotting running time of algorithms.

--- scripts/run_all.py
+++ scripts/run_all.py
@@ -14,9 +14,9 @@ from maheu_group_project.solution.verifying import verify_solution
 from maheu_group_project.solution.evaluate import objective_function
 
 # This is the default configuration of the script. It can be overridden by command line arguments.
-SOLVERS: list[SolverType] = [SolverType.GREEDY]
+SOLVERS: list[SolverType] = [SolverType.GREEDY_CANDIDATE_PATHS]
 DETERMINISTIC = False
-DATASET_INDICES = [2]
+DATASET_INDICES = [1, 2, 3, 4]
 
 
 def run_on_all_data_from_first_dataset():
@@ -31,53 +31,61 @@ def run_on_all_data_from_first_dataset():
             dataset_dir = f"CaseMaHeu25_0{dataset_index}"
             result_filename = f"{current_time}_Case_0{dataset_index}_{deterministic_name_tag}_{solver}"
             with open(os.path.join(results_dir, result_filename + "_result.txt"), 'w') as result_file:
-                with open(os.path.join(results_dir, result_filename + "_result_pretty.txt"),
-                          'w') as pretty_result_file:
-                    data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', dataset_dir))
-                    pattern = os.path.join(data_dir, 'realised_capacity_data_*.csv')
-                    files = sorted(glob.glob(pattern))
-                    for file in files:
-                        start_time = time.time()
-                        # Get solutions from the solver
-                        if DETERMINISTIC:
-                            vehicle_assignments, truck_assignments, _, vehicles, trucks_realised, _ = solve_deterministically_and_return_data(
-                                solver,
-                                dataset_dir,
-                                os.path.basename(file)
-                            )
-                        else:
-                            vehicle_assignments, truck_assignments, _, vehicles, trucks_realised, _ = solve_real_time_and_return_data(
-                                solver,
-                                dataset_dir,
-                                os.path.basename(file)
-                            )
-                        end_time = time.time() - start_time
+                with open(os.path.join(results_dir, result_filename + "_running_time.txt"), 'w') as running_time_file:
+                    with open(os.path.join(results_dir, result_filename + "_result_pretty.txt"),
+                              'w') as pretty_result_file:
+                        data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'data', dataset_dir))
+                        pattern = os.path.join(data_dir, 'realised_capacity_data_*.csv')
+                        files = sorted(glob.glob(pattern))
+                        for file in files:
+                            start_time = time.time()
+                            # Get solutions from the solver
+                            if DETERMINISTIC:
+                                vehicle_assignments, truck_assignments, _, vehicles, trucks_realised, _ = solve_deterministically_and_return_data(
+                                    solver,
+                                    dataset_dir,
+                                    os.path.basename(file)
+                                )
+                            else:
+                                vehicle_assignments, truck_assignments, _, vehicles, trucks_realised, _ = solve_real_time_and_return_data(
+                                    solver,
+                                    dataset_dir,
+                                    os.path.basename(file)
+                                )
+                            end_time = time.time() - start_time
 
-                        # Verify the solution
-                        is_valid = verify_solution(vehicles, vehicle_assignments, trucks_realised, truck_assignments)
-                        number_of_vehicles_that_did_not_arrived = 0
-                        match is_valid:
-                            case bool():
-                                assert is_valid, "The solution is invalid"
-                            case int():
-                                number_of_vehicles_that_did_not_arrived = is_valid
-                            case _:
-                                raise TypeError(f"Unexpected type of is_valid: {type(is_valid)}")
+                            # Verify the solution
+                            is_valid = verify_solution(vehicles, vehicle_assignments, trucks_realised, truck_assignments)
+                            number_of_vehicles_that_did_not_arrived = 0
+                            match is_valid:
+                                case bool():
+                                    assert is_valid, "The solution is invalid"
+                                case int():
+                                    number_of_vehicles_that_did_not_arrived = is_valid
+                                case _:
+                                    raise TypeError(f"Unexpected type of is_valid: {type(is_valid)}")
 
-                        # Compute the cost of the solution
-                        cost = objective_function(vehicle_assignments, truck_assignments, trucks_realised)
+                            # Compute the cost of the solution
+                            cost = objective_function(vehicle_assignments, truck_assignments, trucks_realised)
+                            running_time = f"{end_time:.2f}"
 
-                        # Write the results to the files
-                        output = f"Cost of solution for {dataset_dir}/{os.path.basename(file)}: {cost:.2f} \n"
-                        if number_of_vehicles_that_did_not_arrived != 0:
-                            output += f"Number of vehicles that did not arrive: {number_of_vehicles_that_did_not_arrived} \n"
-                        result_file.write(output)
-                        if number_of_vehicles_that_did_not_arrived == 0:
-                            output += f"Number of vehicles that did not arrive: {number_of_vehicles_that_did_not_arrived} \n"
-                        metrics = get_pretty_metrics(trucks_realised, truck_assignments, vehicle_assignments)
-                        output += metrics + "\n" + f"Running time in seconds: {end_time:.2f}" + "\n" + "\n"
-                        pretty_result_file.write(output)
-                        print(output)
+                            # Write the results to the files
+                            # First to individual files
+                            output = f"Cost of solution for {dataset_dir}/{os.path.basename(file)}: {cost:.2f} \n"
+                            if number_of_vehicles_that_did_not_arrived != 0:
+                                output += f"Number of vehicles that did not arrive: {number_of_vehicles_that_did_not_arrived} \n"
+                            result_file.write(output)
+
+                            running_time_output = f"Running time for {dataset_dir}/{os.path.basename(file)}: {running_time} \n"
+                            running_time_file.write(running_time_output)
+
+                            # And then to the pretty result file
+                            if number_of_vehicles_that_did_not_arrived == 0:
+                                output += f"Number of vehicles that did not arrive: {number_of_vehicles_that_did_not_arrived} \n"
+                            metrics = get_pretty_metrics(trucks_realised, truck_assignments, vehicle_assignments)
+                            output += metrics + "\n" + f"Running time in seconds: {running_time}" + "\n" + "\n"
+                            pretty_result_file.write(output)
+                            print(output)
 
 
 def parse_args() -> argparse.Namespace:
