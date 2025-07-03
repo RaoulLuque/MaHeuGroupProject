7ca810369b8d41b777082eabe34ecf704378985e
Raoul Luqué <raoulsluque@gmail.com>
2025-07-03 18:40:05 +0200

    Add copying of stdout.txt to copy_from_hpc.sh

--- scripts/copy_from_hpc.sh
+++ scripts/copy_from_hpc.sh
@@ -1,2 +1,3 @@
+scp -r xh700552@copy23-1.hpc.itc.rwth-aachen.de:/hpcwork/xh700552/MaHeuGroupProject/stdout.txt ./results/hpc_results/stdout.txt
 scp -r xh700552@copy23-1.hpc.itc.rwth-aachen.de:/hpcwork/xh700552/MaHeuGroupProject/results/real_time ./results/hpc_results/real_time
 scp -r xh700552@copy23-1.hpc.itc.rwth-aachen.de:/hpcwork/xh700552/MaHeuGroupProject/results/deterministic ./results/hpc_results/deterministic
