5e69656e557c885271319e0b2070385736f34414
Raoul Luqué <raoulsluque@gmail.com>
2025-07-08 20:10:45 +0200

    Add quantile_capacity function to compute truck capacity quantiles

---
+++ src/maheu_group_project/uncertainty/quantile.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+from maheu_group_project.solution.encoding import Location, Truck
+from maheu_group_project.uncertainty.history_data_handling import Weekday
+
+
+def quantile_capacity(
+    truck_history: dict[tuple[Weekday, Location, Location, int], list[Truck]],
+    quantile: float
+) -> dict[tuple[Weekday, Location, Location, int], float]:
+    """
+    Computes the capacity such that the provided quantile of capacities from trucks in truck_history have a higher capacity.
+
+    Args:
+        truck_history (dict[tuple[Weekday, Location, Location, int], list[Truck]]):
+            A dictionary where the key is a tuple of weekday, start and end locations, and truck identifier. The value is a list of trucks for that combination.
+        quantile (float): The quantile to compute (e.g., 0.95 for the 95th percentile).
+
+    Returns:
+        dict[tuple[Weekday, Location, Location, int], float]: A dictionary where the key is a tuple of weekday,
+            start and end locations, and truck identifier. The value is the quantile of truck capacities for that combination.
+    """
+    # Convert to the right quantile for higher capacities
+    quantile = 1 - quantile
+    quantile_capacity_res: dict[tuple[Weekday, Location, Location, int], float] = {}
+    for dict_key, trucks in truck_history.items():
+        capacities = [truck.capacity for truck in trucks]
+        quantile_value = float(np.quantile(capacities, quantile))
+        quantile_capacity_res[dict_key] = quantile_value
+    return quantile_capacity_res
