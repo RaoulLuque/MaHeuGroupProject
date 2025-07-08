33ab9b921463d25cdf137774f52ebfd53f17a749
Raoul Luqué <raoulsluque@gmail.com>
2025-07-08 19:21:55 +0200

    Adapt history data handling to return dict of history data by id, segment and weekday

--- src/maheu_group_project/uncertainty/history_data_handling.py
+++ src/maheu_group_project/uncertainty/history_data_handling.py
@@ -1,7 +1,7 @@
 from datetime import date
 from enum import Enum
 
-from maheu_group_project.solution.encoding import Truck, TruckIdentifier
+from maheu_group_project.solution.encoding import Truck, TruckIdentifier, Location
 
 
 class Weekday(Enum):
@@ -30,19 +30,30 @@ def get_weekday_from_date(date_in_datetime: date) -> Weekday:
     return Weekday(date_in_datetime.isoweekday())
 
 
-def history_data_by_weekday(trucks_history: dict[TruckIdentifier, Truck]) -> dict[Weekday, list[Truck]]:
+def history_data_by_id_segment_and_weekday(trucks_history: dict[TruckIdentifier, Truck]) -> dict[Weekday, dict[tuple[Location, Location], dict[int, list[Truck]]]]:
     """
-    Groups the truck history data by the weekday of their departures.
+    Groups the truck history data by weekday, segment (start and end locations), and truck identifier.
 
     Args:
         trucks_history (dict[TruckIdentifier, Truck]): The truck history data.
 
     Returns:
-        dict[datetime.date, list[Truck]]: A dictionary where keys are weekdays and values are lists of Trucks which depart on that weekday.
+        dict[Weekday, dict[tuple[Location, Location], dict[int, list[Truck]]]]:
+            A nested dictionary where the first key is the weekday, the second key is a tuple of start and end locations,
+            and the third key is the truck number. The value is a list of trucks for that combination of
+            weekday, segment and truck number.
     """
-    history_by_day: dict[Weekday, list[Truck]] = {day: [] for day in Weekday}
+    history_by_day: dict[Weekday, dict[tuple[Location, Location], dict[int, list[Truck]]]] = {}
     print(history_by_day)
     for truck in trucks_history.values():
         weekday = get_weekday_from_date(truck.departure_date)
-        history_by_day[weekday].append(truck)
+        segment = (truck.start_location, truck.end_location)
+        id = truck.truck_number
+        if weekday not in history_by_day:
+            history_by_day[weekday] = {}
+        if segment not in history_by_day[weekday]:
+            history_by_day[weekday][segment] = {}
+        if id not in history_by_day[weekday][segment]:
+            history_by_day[weekday][segment][id] = []
+        history_by_day[weekday][segment][id].append(truck)
     return history_by_day
