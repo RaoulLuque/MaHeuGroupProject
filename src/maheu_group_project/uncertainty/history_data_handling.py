237fb83762ab9848968def82fefecdb0ebe7b836
Raoul Luqué <raoulsluque@gmail.com>
2025-07-08 19:29:43 +0200

    Adjust handling of history data again

--- src/maheu_group_project/uncertainty/history_data_handling.py
+++ src/maheu_group_project/uncertainty/history_data_handling.py
@@ -30,7 +30,21 @@ def get_weekday_from_date(date_in_datetime: date) -> Weekday:
     return Weekday(date_in_datetime.isoweekday())
 
 
-def history_data_by_id_segment_and_weekday(trucks_history: dict[TruckIdentifier, Truck]) -> dict[Weekday, dict[tuple[Location, Location], dict[int, list[Truck]]]]:
+def truck_to_weekday_segment_and_id_key(truck: Truck) -> tuple[Weekday, Location, Location, int]:
+    """
+    Creates a key for the truck based on its weekday, start and end locations, and identifier.
+
+    Args:
+        truck (Truck): The truck object.
+
+    Returns:
+        tuple[Weekday, Location, Location, int]: A tuple containing the weekday, start location,
+            end location, and truck identifier.
+    """
+    return get_weekday_from_date(truck.departure_date), truck.start_location, truck.end_location, id(truck)
+
+
+def history_data_by_id_segment_and_weekday(trucks_history: dict[TruckIdentifier, Truck]) -> dict[tuple[Weekday, Location, Location, int], list[Truck]]:
     """
     Groups the truck history data by weekday, segment (start and end locations), and truck identifier.
 
@@ -39,21 +53,14 @@ def history_data_by_id_segment_and_weekday(trucks_history: dict[TruckIdentifier,
 
     Returns:
         dict[Weekday, dict[tuple[Location, Location], dict[int, list[Truck]]]]:
-            A nested dictionary where the first key is the weekday, the second key is a tuple of start and end locations,
-            and the third key is the truck number. The value is a list of trucks for that combination of
+            A dictionary where key is a tuple of weekday, start and end locations,
+            and the truck number. The value is a list of trucks for that combination of
             weekday, segment and truck number.
     """
-    history_by_day: dict[Weekday, dict[tuple[Location, Location], dict[int, list[Truck]]]] = {}
-    print(history_by_day)
+    history_by_day: dict[tuple[Weekday, Location, Location, int], list[Truck]] = {}
     for truck in trucks_history.values():
-        weekday = get_weekday_from_date(truck.departure_date)
-        segment = (truck.start_location, truck.end_location)
-        id = truck.truck_number
-        if weekday not in history_by_day:
-            history_by_day[weekday] = {}
-        if segment not in history_by_day[weekday]:
-            history_by_day[weekday][segment] = {}
-        if id not in history_by_day[weekday][segment]:
-            history_by_day[weekday][segment][id] = []
-        history_by_day[weekday][segment][id].append(truck)
+        dict_key = truck_to_weekday_segment_and_id_key(truck)
+        if dict_key not in history_by_day:
+            history_by_day[dict_key] = []
+        history_by_day[dict_key].append(truck)
     return history_by_day
