443d08e59685adb0cb933f68ad56c02f56ce8f8c
Raoul Luqué <raoulsluque@gmail.com>
2025-07-10 09:50:50 +0200

    Adjust hpc scripts for quantile 0.5 run

--- scripts/hpc_run_mip_real_time_two.sh
+++ scripts/hpc_run_mip_real_time_two.sh
@@ -25,4 +25,4 @@ pip install networkx==3.5
 pip install matplotlib==3.10.3
 
 ### Run the script (once for deterministic and once for real-time)
-python3 scripts/run_all.py --deterministic FALSE --solvers FLOW_MIP --dataset_indices 2 4 --quantile_value 0.0
+python3 scripts/run_all.py --deterministic FALSE --solvers FLOW_MIP --dataset_indices 2 4 --quantile_value 0.5
